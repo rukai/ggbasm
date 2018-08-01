cc0932daf7179f204a05c8a65a7590a38b3d0c20
Rukai <rubickent@gmail.com>
2018-08-02 00:50:56 +1000

    Add new project command

--- sgbasm/src/main.rs
+++ sgbasm/src/main.rs
@@ -2,6 +2,7 @@
 
 use std::env;
 use std::fs::File;
+use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
 
@@ -12,10 +13,13 @@ use sgbasm_lib;
 
 #[derive(StructOpt)]
 struct Options {
-    #[structopt(parse(from_os_str))]
+    /// filename to store the ROM in
+    #[structopt(short="o", long="output", parse(from_os_str))]
     output: Option<PathBuf>,
-    #[structopt(parse(from_os_str))]
-    new_project: Option<PathBuf>,
+
+    /// Create a new sgbasm project
+    #[structopt(short="n", long="new", parse(from_os_str))]
+    new: Option<PathBuf>,
 }
 
 fn main() {
@@ -27,12 +31,24 @@ fn main() {
 fn run() -> Result<(), Error> {
     let options = Options::from_args();
 
-    let source_files = sgbasm_lib::source_files()?;
-    let rom = sgbasm_lib::source_to_rom(&source_files)?;
+    if let Some(new) = options.new {
+        fs::create_dir_all(&new)?;
+        File::create(new.join("main.asm"))?;
+        println!("Created new project: {:?}", new);
+    }
+    else {
+        let source_files = sgbasm_lib::source_files()?;
+        let rom = sgbasm_lib::source_to_rom(&source_files)?;
 
-    let output = options.output.unwrap_or(env::current_dir()?.join("out.gb"));
-    let mut output_file = File::create(output)?;
-    output_file.write(&rom)?;
+        let output = options.output.unwrap_or(env::current_dir()?.join("out.gb"));
+        let mut output_file = File::create(&output)?;
+        output_file.write(&rom)?;
+        if let Some(name) = output.file_name() {
+            if let Some(name) = name.to_str() {
+                println!("Compiled project to: {}", name);
+            }
+        }
+    }
 
     Ok(())
 }
